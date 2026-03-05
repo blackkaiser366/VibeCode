7cc3b3c5b4571f6c84072b6fe331afe5ac41e997
blackkaiser366 <theshieldrollin1@gmail.com>
2026-03-05 14:11:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Selection Sort/selection-sort.c
@@ -0,0 +1,16 @@
+void selectionSort(int arr[], int n) {
+    // Code here
+    for (int i = 0; i < n - 1; i++) {
+        int min_idx = i;
+        // Find the minimum element in the unsorted portion
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
