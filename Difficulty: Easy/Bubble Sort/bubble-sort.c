b7f832b0131e945a5536939090f108ef98099af8
blackkaiser366 <theshieldrollin1@gmail.com>
2026-03-05 14:09:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bubble Sort/bubble-sort.c
@@ -0,0 +1,18 @@
+
+void bubbleSort(int* arr, int n) {
+    // Code here
+        for (int i = 0; i < n - 1; i++) {
+        int swapped = 0;
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+                swapped = 1;
+            }
+        }
+        if (!swapped) break; 
+
+}
+}
